f3d91dd4f65fb8d451386c4466afc99ddeb1afd4
sheac <sheaconlonswain@gmail.com>
2026-04-20 16:05:19 +0100

    Changed from PyGame to PyQt

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+from ide import IDE
+from memory import Memory
+from processor import Processor
+from state import MachineState
+
+if __name__ == "__main__":
+    proc = Processor()
+    ram = Memory(size = 0x10000 * 2) # 64KB * 2
+    state = MachineState(proc, ram)
+
+    app = QApplication(sys.argv)
+    window = IDE(proc, ram)
+    window.show()
+
+    sys.exit(app.exec_())
--- memory.py
+++ memory.py
@@ -49,7 +49,7 @@ class Memory:
         :param address: The address to read from
         :return: The value at given address
         """
-        if 0x0000 < address > mem.size:
+        if 0x0000 < address or mem.size < address:
             raise ValueError("Memory address is not valid")
 
         if mem.is_banked and address == mem.bank_register:
@@ -66,7 +66,7 @@ class Memory:
         :param value: The value ot write to the address
         :return: None
         """
-        if 0x0000 < address > mem.size:
+        if 0x0000 < address or mem.size < address:
             raise ValueError("Memory address is not valid")
         if value.bit_length() > 8:
             raise ValueError("Value too large")
